1cdebdabf4fab2a44a5929a2fbacb866b078c8a7
adcpm <fabien@bonustrack.co>
2020-07-26 23:08:21 +0700

    Default to livenet

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,8 +1,7 @@
 import config from '@/config.json';
 
-let network = process.env.VUE_APP_NETWORK || 'testnet';
+let network = process.env.VUE_APP_NETWORK || 'livenet';
 const domainName = window.location.hostname;
-if (domainName === 'oswap.io') network = 'livenet';
 if (domainName === 'testnet.oswap.io') network = 'testnet';
 
 export default config[network];
