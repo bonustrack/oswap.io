c540a53eb25a7adc27d0183f8e9f3b8411a9ec29
Tarmo <tarmo888@hotmail.com>
2020-09-30 01:47:28 +0300

    warn about camelCase

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
   rules: {
-    "camelcase": [2, {"properties": "always"}],
+    camelcase: ['warn', { properties: 'always' }],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
