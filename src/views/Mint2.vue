066f96b3115a65e318b1192eca2293c5434006ef
Tarmo <tarmo888@hotmail.com>
2020-10-26 04:22:48 +0200

    exchange rate when depositing one token

--- src/views/Mint2.vue
+++ src/views/Mint2.vue
@@ -8,24 +8,42 @@
         class="d-flex"
       >
         <div class="flex-auto">
-          <label for="amount" class="d-block">
+          <label for="input" class="d-block">
             Deposit
-            <LabelBalance :asset="asset" @select="setAmount" />
+            <LabelBalance :asset="inputAsset" @select="selectAmount" />
           </label>
-          <InputAmount id="amount" v-model="amount" :asset="asset" />
+          <InputAmount
+            id="input"
+            v-model="inputAmount"
+            @change="updateOutputAmount"
+            :asset="inputAsset"
+          />
         </div>
         <div class="text-right mt-4 ml-4">
-          <ButtonSelectToken :values="[selectedPool.asset0, selectedPool.asset1]" v-model="asset" />
+          <ButtonSelectToken
+            :values="[selectedPool.asset0, selectedPool.asset1]"
+            v-model="inputAsset"
+          />
         </div>
       </Box>
-      <Box>
+      <Box v-if="rate">
+        <p class="text-white float-right m-0">
+          1 <Ticker :asset="outputAsset" /> = {{ rate }} <Ticker :asset="inputAsset" />
+        </p>
+        <label for="to">Exchange rate</label>
+      </Box>
+      <Box v-if="rate">
         <p class="text-white m-0">
-          Adding liquidity with just one token is same as swapping a token, and then automatically
-          adding liquidity with both tokens. Use with caution!
+          Adding liquidity with just one token is same as swapping half of the tokens, and then
+          automatically adding liquidity with both tokens. Use with caution!
         </p>
       </Box>
       <div class="text-center">
-        <button class="btn-submit px-6 rounded-2 mb-3" type="submit" :disabled="!asset || !amount">
+        <button
+          class="btn-submit px-6 rounded-2 mb-3"
+          type="submit"
+          :disabled="!inputAsset || !inputAmount"
+        >
           Add liquidity
         </button>
       </div>
@@ -34,32 +52,77 @@
 </template>
 
 <script>
-import { generateUri } from '@/helpers/_oswap';
+import Trade from '@/helpers/_oswap/trade';
+import Factory from '@/helpers/_oswap/factory';
+import { generateUri, toString } from '@/helpers/_oswap';
 
 export default {
   data() {
     return {
       selectedPool: false,
-      asset: '',
+      trade: false,
+      inputAmount: '',
+      inputAsset: '',
       poolAddress: this.$route.params.poolAddress,
-      amount: ''
+      rate: 0
     };
   },
   watch: {
+    async inputAmount(value, oldValue) {
+      if (value !== oldValue) {
+        this.updateOutputAmount();
+      }
+    },
+    async inputAsset(value, oldValue) {
+      if (value !== oldValue) {
+        this.outputAsset =
+          this.inputAsset === this.selectedPool.asset0
+            ? this.selectedPool.asset1
+            : this.selectedPool.asset0;
+        await this.init();
+        this.updateOutputAmount();
+      }
+    },
     async selectedPool(value, oldValue) {
       if (value !== oldValue) {
-        this.amount = '';
+        this.inputAmount = '';
+        this.rate = 0;
       }
     }
   },
   methods: {
-    setAmount(amount) {
-      this.amount = amount;
+    selectAmount(amount) {
+      this.inputAmount = amount;
+      this.updateOutputAmount();
+    },
+    getDecimals(assetId) {
+      return this.settings.decimals[assetId] || 0;
+    },
+    updateRate() {
+      if (!this.inputAsset || !this.outputAsset) {
+        this.rate = 0;
+        return;
+      }
+      const inputAmount = toString(this.inputAmount / 2, this.getDecimals(this.inputAsset));
+      const outputAmount = toString(this.outputAmount, this.getDecimals(this.outputAsset));
+      const rate = parseFloat((inputAmount / outputAmount).toFixed(6));
+      if (rate <= 0 || rate === Infinity) {
+        this.rate = 0;
+        return;
+      }
+      this.rate = rate;
+    },
+    async init() {
+      if (!this.inputAsset || !this.outputAsset) return;
+      const settings = this.settings;
+      const factory = new Factory(settings.pools, settings.pairs);
+      this.trade = new Trade(factory, this.inputAsset, this.outputAsset);
+      await this.trade.init();
     },
     handleSubmit() {
       const data = { mint: '1' };
       const address = this.selectedPool.address;
-      const url = generateUri(address, data, this.amount, this.asset);
+      const url = generateUri(address, data, this.inputAmount, this.inputAsset);
       if (navigator.userAgent.indexOf('Firefox') != -1) {
         const opener = window.open(url, '', 'width=1,height=1,resizable=no');
         setTimeout(function() {
@@ -68,7 +131,13 @@ export default {
       } else {
         location.href = url;
       }
-    }
+    },
+    updateOutputAmount() {
+      if (!this.inputAsset || !this.outputAsset) return;
+      if (this.inputAmount)
+        this.outputAmount = this.trade.getAmountBought(this.inputAmount / 2) || '';
+      this.updateRate();
+    },
   }
 };
 </script>
